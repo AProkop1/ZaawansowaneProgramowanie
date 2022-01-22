aa8265826a34518dfc73421303b35993ce5545fd
AProkop1 <agnieszka.prokop1@edu.uekat.pl>
2022-01-22 14:15:59 +0100

    projekt

---
+++ Detect.py
@@ -0,0 +1,39 @@
+import cv2
+import imutils
+import numpy as np
+from imutils.object_detection import non_max_suppression
+
+
+def detect_people(file_name: str) -> None:
+    # Initializing the HOG person detection
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    # Reading image
+    image = cv2.imread(file_name)
+
+    # Resizing the Image
+    image = imutils.resize(image, width=min(800, image.shape[1]))
+
+    # Detecting all the regions in the image with pedestrians
+    (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
+                                            padding=(8, 8), scale=1.05)
+
+    # Drawing boxes
+    for (x, y, w, h) in rects:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    # Applying non-maxima suppression
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+    # Drawing thee final boxes
+    for (xA, yA, xB, yB) in pick:
+        cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+    # Showing information about number of people detected
+    print("[FOUND] : {} people.".format(len(pick)))
+
+    # Showing output images
+    cv2.imshow("People detection", image)
+    cv2.waitKey(0)
--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-from models.Dietetyk import Dietetyk
-from models.Pacjent import Pacjent
-from models.Dieta import Dieta
-from models.Zamówienie import Zamowienie
+import glob
+from Detect import detect_people
 
-dieta1 = Dieta("Dieta Vegan", "Diety Roślinne", 49.99, 1800)
-dieta2 = Dieta("Dieta Sport", "Diety z wysoką zawartością białka", 59.99, 3000)
-diety = [dieta1, dieta2]
-pacjent = Pacjent("Jan", "Kowalski", 29, "ul. Adamskiego 14/2 Katowice")
-dietetyk = Dietetyk("Mariusz", "Nowak", "BodyChef", 651736)
-
-zamowienie = Zamowienie(dietetyk, pacjent, diety, "ul. Adamskiego 14/2 Katowice")
-
-print(zamowienie)
-
-print(zamowienie.cena_zamowienia())
-print(zamowienie.kcal_zamowienia())
+# loop through the folder with images
+for file_name in glob.iglob('images/*', recursive=True):
+    detect_people(file_name)
